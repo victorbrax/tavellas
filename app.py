112946d37c7cdaa0706c13477afd4f098456936e
Víctor Gomes <joaogomespcl@gmail.com>
2023-04-16 19:10:07 -0300

    reboot

--- app.py
+++ app.py
@@ -5,6 +5,5 @@ from apps import register_blueprints
 app = Flask(__name__)
 register_blueprints(app)
 
-
 if __name__ == "__main__":
     app.run(debug=True)
--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -2,6 +2,17 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__, static_folder="static", template_folder="templates")
 
-@auth.route('/')
+@auth.route('/login')
 def auth_page():
-    return render_template('accounts/login.html')
+    return render_template('accounts/login.html', 
+                           inspiration_image='wallpaper1')
+
+@auth.route('/register')
+def register_page():
+    return render_template('accounts/register.html', 
+                           inspiration_image='wallpaper2')
+
+@auth.route('/forgot')
+def forgot_page():
+    return render_template('accounts/forgot_pass.html', 
+                           inspiration_image='wallpaper3')
