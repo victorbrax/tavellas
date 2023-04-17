94a3f685a551e2572aa184a875afe946d9fe40b9
Víctor Gomes <joaogomespcl@gmail.com>
2023-04-16 22:04:11 -0300

    adicao de detalhes

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 register_blueprints(app)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8000)
--- apps/__init__.py
+++ apps/__init__.py
@@ -5,7 +5,7 @@ from apps.users import user
 
 def register_blueprints(app):
 
-    app.register_blueprint(auth, url_prefix="/auth")
+    app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(user, url_prefix="/user")
     app.register_blueprint(admin, url_prefix="/admin")
     
