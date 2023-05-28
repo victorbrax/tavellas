f606e5f7c1819570d459a6b573f6958f61f4ad61
DerekSilva <viniciusdereck39@gmail.com>
2023-05-28 00:52:46 -0300

    finalizacao da formatacao do input para moeda

--- static/js/numberFloat.js
+++ static/js/numberFloat.js
@@ -1,18 +1,90 @@
+/**
+ * Elemento para transformar o preço
+ */
 const price = document.querySelector("#price")
 
-price.addEventListener("keyup", (letra) => {
-    if (Number(letra.key)) {   
+/**
+ * Armazena os valores digitados pelo usuário
+ */
+numbers = []
 
-        aoba = price.value.replace(/[^0-9,-]/g, '').replace(',', '').replace('00', '')
-        value = Number(aoba).toLocaleString('pt-br', {style: 'currency', currency: 'BRL'});
-        price.value = value
-        console.log(aoba)
+/**
+ * Evento ao pressinar uma tecla
+ */
+price.addEventListener("keyup", (letra) => {
+
+    /* Zera o array caso o input esteja vazio */
+    if (price.value === '') {
+        numbers = []
+    }
     
+    /* Insere apenas números */
+    if (Number(letra.key) || letra.key === '0') {   
+        
+        numbers.push(letra.key)
+        this.transformString(numbers)
+    }
+    
+    /* Exclui um valor */
+    if (letra.key === 'Backspace') {
+        numbers.pop()
+        this.transformString(numbers)
+    }
+
+    /* Capacidade máxima de caracteres */
+    if (price.value.length === 12) {
+        return false
     }
 })
 
+/**
+ * Realiza a trasformação de um array para string
+ * 
+ * @author Dereck Silva
+ * @since 27/05/2023
+ * @param { array } numbers 
+ */
 function transformString(numbers) {
-    let number = numbers.split().filter((letter) => { return letter  })
+    let number = numbers.toString()
+    let priceService  = ''
+    for (let num in number) {
+        priceService += number[num] != ',' ? number[num] : ''
+    }
     
-   return number.toString()
+    priceService = this.formatPrice(priceService)
+    price.value = `R$ ${priceService}`
+}
+
+/**
+ * Formata o preço com base na quantidade de caracteres
+ * 
+ * @author Dereck Silva
+ * @since 27/05/2023
+ * @param { string } price 
+ * @returns { string } price
+ */
+function formatPrice(price) {
+    switch(price.length) {
+        case 1:
+        case 2:
+            price = price.replace(/(\d{0})(\d{2})/, '$1.$2')
+            break
+        case 3:
+            price = price.replace(/(\d{1})(\d{2})/, '$1.$2')
+            break
+        case 4:
+            price = price.replace(/(\d{2})(\d{2})/, '$1.$2')
+            break
+        case 5:
+            price = price.replace(/(\d{3})(\d{2})/, '$1.$2')
+            break
+        case 6:
+            price = price.replace(/(\d{1})(\d{3})(\d{2})/, '$1,$2.$3')
+            break
+        case 7:
+            price = price.replace(/(\d{2})(\d{3})(\d{2})/, '$1,$2.$3')
+            break
+    }
+
+    return price
 }
