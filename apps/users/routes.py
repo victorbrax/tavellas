47f50a0e7936256557e68a76f0b449a35d95cda2
DerekSilva <viniciusdereck39@gmail.com>
2023-05-28 14:45:40 -0300

    parcial

--- apps/users/routes.py
+++ apps/users/routes.py
@@ -12,7 +12,10 @@ def user_suport():
 
 @user.route('/demandas')
 def user_demands():
-    return render_template('user/demandas.html', aguardando  = {}, 
+    return render_template('user/demandas.html', aguardando  = {1542 : {'id': 1542, 'modelo': 'dereck', 'estado': 23, 'condicao' : 'masculino',
+                                                                         'aro': 'teste', 'quadro' : 'teste', 'cor' : 'cinza', 'obs': 'salvesalvelsavelsal',
+                                                                         'resp': 'teste', 'cliente': 'teste'
+                                                                         },}, 
                                                  emReparo    = {}, 
                                                  finalizadas = {}, 
                                                  entregues   = {})
