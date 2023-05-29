08dac2ff82f063817cfc3bbfd475a041906930c1
Víctor Gomes <joaogomespcl@gmail.com>
2023-05-29 03:41:42 -0300

    fix css2

--- apps/admin/routes.py
+++ apps/admin/routes.py
@@ -2,10 +2,7 @@ from flask import Blueprint, render_template
 
 admin = Blueprint('admin', __name__, static_folder="static", template_folder="templates")
 
+@admin.route('/')
 @admin.route('/home')
 def home_page():
-    return render_template('admin/admin.html')
-
-@admin.route('/newbase')
-def newbase():
-    return render_template('admin/newbase.html')
+    return render_template('admin/index.html')
